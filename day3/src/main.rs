fe729ffc00bc797edb5eaee78c7d84753bf37e02
Nathan Gunderson <npgunderson@gmail.com>
2025-12-31 13:21:35 -0600

    feat: wip day3

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,3 +1,33 @@
+use std::fs;
+
+fn read_file(name: &str) -> String
+{
+    let input = 
+        fs::read_to_string(
+            format!(
+                "{}/{}",
+                env!("CARGO_MANIFEST_DIR"),
+                name))
+        .expect("file not read!");
+    input.trim().to_string()
+}
+
 fn main() {
     println!("===Day 3!===");
+
+    let input = read_file("example.txt");
+
+    for battery_bank in input.lines() {
+        // find largest battery ignoring last slot, if multiple, pick
+        // the first index
+        // then look for the largest battery after the first index
+        // Then combine the two chars and add to running total
+
+        for battery in battery_bank.chars() {
+            assert!(('0' <= battery) && (battery <= '9'),
+              "Battery not in range! ({battery})");
+
+            
+        }
+    }
 }
