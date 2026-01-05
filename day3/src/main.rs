175ace96f1dca5ce2a0144471932ea4950345b0e
Nathan Gunderson <npgunderson@gmail.com>
2026-01-04 18:36:47 -0600

    refactor: Create new function to find n largest joltage

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -34,12 +34,64 @@ fn second_battery(battery_bank: &str, first_index: usize) -> char {
     second_battery.expect("Second battery not found!")
 }
 
+fn find_max(battery_bank: &str, previous_index: Option<usize>, digit_position: u8) -> (usize, u32) {
+    // Start the search right after the previously identified largest number
+    // Also ignore the last digits which are smaller than the digit position
+    // we're searching for.
+    let start_index = if let Some(value) = previous_index {
+        value + 1
+    } else {
+        0
+    };
+    let last_battery_index = battery_bank.len() - 1;
+    // For example, digit position 1, should point at very last element, so
+    // last battery index - (1 - 1) = last battery index
+    let final_index = last_battery_index - usize::from(digit_position - 1);
+    let slice_to_search = &battery_bank[start_index..=final_index];
+
+    let mut max_battery = char::MIN;
+    let mut max_battery_index = None;
+
+    // Find the largest char. If there are multiple, use the first index
+    // so we can find the largest possible joltage.
+    for (index, battery) in slice_to_search.chars().enumerate() {
+        if battery > max_battery {
+            max_battery = battery;
+            max_battery_index = Some(index);
+        }
+    }
+    //println!("{} found for {}", max_battery, slice_to_search);
+    (
+        max_battery_index.expect("max not found!"),
+        max_battery.to_digit(10).unwrap(),
+    )
+}
+
+fn highest_n_digit_joltage(battery_bank: &str, num_digits: u8) -> u64 {
+    assert!(num_digits > 0 && num_digits <= 12, "Size invalid!");
+
+    let mut previous_index = None;
+    let mut max_joltage: u64 = 0;
+
+    for digit_position in (1..=num_digits).rev() {
+        let (index, value) = find_max(battery_bank, previous_index, digit_position);
+
+        previous_index = Some(index);
+
+        max_joltage = max_joltage + (u64::from(value) * 10_u64.pow(u32::from(digit_position) - 1));
+    }
+    //println!("Joltage found ({max_joltage})");
+
+    max_joltage
+}
+
 fn main() {
     println!("===Day 3!===");
 
     let input = read_file("real.txt");
 
-    let mut part1_sum = 0;
+    let mut part1_sum: u64 = 0;
+    let mut part2_sum: u64 = 0;
 
     for battery_bank in input.lines() {
         // find largest battery ignoring last slot, if multiple, pick
@@ -55,8 +107,11 @@ fn main() {
 
         //println!("line is ({battery_bank}), max ({max_joltage})");
 
-        part1_sum += max_joltage;
+        part1_sum += u64::from(max_joltage);
+
+        part2_sum += highest_n_digit_joltage(battery_bank, 2);
     }
 
     println!("Part1 sum ({part1_sum})");
+    println!("Part2 sum ({part2_sum})");
 }
