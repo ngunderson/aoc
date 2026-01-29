6458495338112912f6f80a4652c40f92bba121ae
Nathan Gunderson <npgunderson@gmail.com>
2026-01-28 22:15:36 -0600

    feat: wip day5

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -1,5 +1,5 @@
-use std::fs;
 use std::collections::HashSet;
+use std::fs;
 
 fn read_file(name: &str) -> String {
     let input = fs::read_to_string(format!("{}/{}", env!("CARGO_MANIFEST_DIR"), name))
@@ -7,6 +7,7 @@ fn read_file(name: &str) -> String {
     input.trim().to_string()
 }
 
+#[derive(Clone)]
 struct Range {
     first: u64,
     second: u64,
@@ -18,24 +19,38 @@ impl Range {
     }
 
     fn len(&self) -> u64 {
-       if self.first == self.second {
-          return 1;
-       }
-       self.second - self.first + 1
+        if self.first == self.second {
+            return 1;
+        }
+        self.second - self.first + 1
     }
+}
+
+fn set_intersects(a: &Range, b: &Range) -> bool {
+    // a is within b or b is within a
+    a.first >= b.first || a.second <= b.second || b.first >= a.first || b.second <= a.second
 }
 
 // A \ B
 // Return the items in `a` that aren't in `b`
 fn set_difference(a: &Range, b: &Range) -> Option<Range> {
-   // options.. 
-   // - no overlap, return a
-   // -
-   // [1, 3] \ [5, 6] = [1, 3]
-   // [1, 3] \ [2, 4] = [1]
-   // [1, 4] \ [3, 4] = [1, 2]
-   // [6, 9] \ [3, 7] = [8, 9]
-   None
+    // options..
+    // - no overlap, return a
+    // -
+    // [1, 3] \ [5, 6] = [1, 3] , less than
+    // [1, 3] \ [2, 4] = [1] , overlap first
+    // [1, 4] \ [3, 4] = [1, 2] , overlap all
+    // [6, 9] \ [3, 7] = [8, 9] , overlap second half
+    // [2, 8] \ [3, 4] = overlap all
+    //   if a.first < b.first || a.first > b.second && a.second > {
+
+    if !set_intersects(&a, &b) {
+        return Some(a.clone());
+    } else {
+      
+    }
+    
+    None
 }
 
 fn main() {
@@ -62,9 +77,9 @@ fn main() {
         } else {
             let ingredient_id: u64 = line.parse().unwrap();
             let fresh = ranges.iter().any(|range| range.contains(ingredient_id));
-	    if fresh {
-	       fresh_ingredient_count += 1;
-	    }
+            if fresh {
+                fresh_ingredient_count += 1;
+            }
         }
     }
 
@@ -73,11 +88,10 @@ fn main() {
     let mut unique_ranges: Vec<Range> = Vec::new();
 
     for range in ranges {
-    	if unique_ranges.len() == 0 {
-	   unique_ranges.push(range);
-	} else {
-	   
-	}
+        if unique_ranges.len() == 0 {
+            unique_ranges.push(range);
+        } else {
+        }
     }
 
     println!("part2, fresh ingredient ids: ");
